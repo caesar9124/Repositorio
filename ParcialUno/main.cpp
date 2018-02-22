4b0654c10455ccc1b475c311d15023e3674864e4
MateoAlg <mateoalmu99@hotmail.com>
2018-02-21 21:51:00 -0500

    Programa1

--- ParcialUno/main.cpp
+++ ParcialUno/main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+#include <iostream> //Biblioteca
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    //Declaracion de variables
+    int Num1=0,Num2=0, Num3=0;
+    //Declaracion datos de usuario
+    cout << "Ingrese un numero"<< endl;
+    cin >> Num1;
+    cout << "Ingrese el segundo numero" <<endl;
+    cin >> Num2;
+    cout << "Ingrese el tercer numero" << endl;
+    cin >> Num3;
+    //Condicional
+    if (Num3= Num1+Num2)
+    {
+       cout<< "El tercer numero es igual a la suma de los dos primeros"<< endl;
+    }
     return 0;
-}
+} //Fin Algoritmo
